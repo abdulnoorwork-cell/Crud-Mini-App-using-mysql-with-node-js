b366eac72bd2dcb41d71be12ba3dbd6c46b0318c
abdulnoor <abdulnoorwork@gmail.com>
2026-02-01 13:37:24 +0500

    save

--- server/api/server.js
+++ server/api/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 ]
 const corsOptions = {
     origin: function (origin, callback) {
-        if(allowedOrigins.indexOf(origin !== -1) || !origin){
+        if (allowedOrigins.indexOf(origin !== -1) || !origin) {
             callback(null, true)
         } else {
             callback(new Error("Not allowed by cors"))
